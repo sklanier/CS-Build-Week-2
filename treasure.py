dbe6220ba6a7048ef414608208e4b7598c93a6d7
Steve Lanier <stevekentlanier@gmail.com>
2019-08-26 13:56:27 -0700

    added room and world classes from Graphs SC. started algorithm Pseudocode.

---
+++ treasure.py
@@ -0,0 +1,20 @@
+import sys
+
+# traversal algorithm
+
+# make a set for graph
+map = {
+  
+}
+# dictionary of rooms
+# make GET
+  # mark room as visited
+  # take note of:
+    # room ID
+    # exits
+    # treasure?
+      # if yes, pick up
+  # 
+
+# make POST with direction to travel
+    
